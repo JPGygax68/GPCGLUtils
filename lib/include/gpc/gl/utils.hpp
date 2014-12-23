e914d14954905a0b9c21e606587a255216fa012d
Jean-Pierre Gygax <gygax@practicomp.ch>
2014-12-24 00:45:17 +0100

    Using #line GLSL directive to improve diagnostic messages in case #define's are prepended to shader code.

--- lib/include/gpc/gl/utils.hpp
+++ lib/include/gpc/gl/utils.hpp
@@ -9,7 +9,8 @@ namespace gpc {
     namespace gl {
     
 #ifdef _DEBUG
-        inline void checkUniformLocation(const char *name, GLuint location) {
+        inline void checkUniformLocation(const char *name, GLuint location) 
+        {
             GLint program = 0;
             EXEC_GL(glGetIntegerv, GL_CURRENT_PROGRAM, &program);
             assert(program > 0);
@@ -20,18 +21,23 @@ namespace gpc {
         inline void checkUniformLocation(GLuint program, const char *name, GLuint index) {}
 #endif
 
-        inline void setUniform(const char *name, GLuint index, GLint value) {
+        inline void setUniform(const char *name, GLuint index, GLint value)
+        {
             checkUniformLocation(name, index);
             EXEC_GL(glUniform1i, index, value);
         }
 
         template <int Count>
-        inline void setUniform(const char *name, GLuint index, const GLfloat *values) {
+        inline void 
+        setUniform(const char *name, GLuint index, const GLfloat *values) 
+        {
             checkUniformLocation(name, index);
             EXEC_GL(glUniform4fv, index, 1, values);
         }
 
-        inline std::string getShaderCompilationLog(GLuint shader) {
+        inline auto
+        getShaderCompilationLog(GLuint shader) -> std::string
+        {
             GLint log_size = 0;
             EXEC_GL(glGetShaderiv, shader, GL_INFO_LOG_LENGTH, &log_size);
             std::string log(log_size, 0);
@@ -41,14 +47,16 @@ namespace gpc {
             return log;
         }
 
-        inline std::string compileShader(GLuint shader, const std::string source, const std::string defines = "") {
+        inline auto
+        compileShader(GLuint shader, const std::string source, const std::string defines = "") -> std::string
+        {
             // NOTE: this method of adding #define's to a shader violates the GLSL specification that says that
             //  the first non-blank, non-comment statement must be #version
             std::vector<const char *> sources;
             std::vector<GLint> lengths;
             if (!defines.empty()) { 
                 sources.push_back(defines.c_str()); lengths.push_back(defines.size()); 
-                sources.push_back("\n"); lengths.push_back(1);
+                sources.push_back("\n#line 1\n"); lengths.push_back(1);
             }
             sources.push_back(source.c_str());
             lengths.push_back(source.size());
