cd3864915462db1dc1220c1568aff0b7651c3826
Jean-Pierre Gygax <gygax@practicomp.ch>
2017-03-18 17:20:17 +0100

    GLEW replaces glbinding (for now)

--- lib/include/gpc/gl/shader_program.hpp
+++ lib/include/gpc/gl/shader_program.hpp
@@ -3,10 +3,6 @@
 #include <string>
 #include <vector>
 
-//#include <glbinding/gl/gl.h>
-//using namespace gl;
-
-#include <gpc/gl/wrappers.hpp>
 
 // DECLARATIONS --------------------------------------------
 
@@ -30,14 +26,15 @@ namespace gpc {
 
 } // ns gpc
 
-// IMPLEMENTATION -----------------------------------------
+
+// Implementation -------------------------------------------
 
 namespace gpc {
 
     namespace gl {
 
         inline auto
-        getShaderCompilationLog(GLuint shader) -> std::string
+            getShaderCompilationLog(GLuint shader) -> std::string
         {
             GLint log_size = 0;
             GL(GetShaderiv, shader, GL_INFO_LOG_LENGTH, &log_size);
@@ -48,29 +45,8 @@ namespace gpc {
             return log;
         }
 
-        inline auto 
-        insertLinesIntoShaderSource(const std::string &source, const std::string &inserts) -> std::string
-        {
-            // Traverse the source line by line
-            for (size_t i = 0, j = 0; (j = source.find("\n", i)) != std::string::npos; i = j + 1)
-            {
-                size_t len = j - i;
-                if (len > 8 && source.substr(i, 8) == "#version")
-                {
-                    // "#version" line found, insert extra lines just after that
-                    // TODO: use "#line" statement to make error messages less confusing
-                    return source.substr(0, j + 1) + inserts + "\n" + source.substr(j + 1);
-                }
-            }
-
-            // No "#version" found -> insert at the beginning
-            return inserts + "\n" + source;
-        }
-
-        /** header_lines: how many lines to skip before inserting define's
-         */
         inline auto
-        compileShader(GLuint shader, const std::string &source, const std::string &defines) -> std::string
+            compileShader(GLuint shader, const std::string &source, const std::string &defines) -> std::string
         {
             // NOTE: this method of adding #define's to a shader violates the GLSL specification that says that
             //  the first non-blank, non-comment statement must be #version
@@ -101,42 +77,14 @@ namespace gpc {
             return buildShaderProgram(vertex_shader, fragment_shader);
         }
 
-        inline void checkShaderCompileStatus(GLuint shader)
-        {
-            GLint status;
-            GL(GetShaderiv, shader, GL_COMPILE_STATUS, &status);
-            if (!status) throw std::runtime_error("shader compile status is not \"successful\"");
-        }
+        inline GLuint buildShaderProgram(GLuint vertex_shader, GLuint fragment_shader) {
 
-        inline void checkShaderProgramLinkStatus(GLuint program)
-        {
-            GLint is_linked = 0;
-            GL(GetProgramiv, program, GL_LINK_STATUS, &is_linked);
-            if (!is_linked) throw std::runtime_error("shader program has not been linked successfully");
-        }
-
-        inline GLuint buildShaderProgram(GLuint vertex_shader, GLuint fragment_shader)
-        {
             GLuint program = GL(CreateProgram);
 
-            if (vertex_shader)
-            {
-                checkShaderCompileStatus(vertex_shader);
-                GL(AttachShader, program, vertex_shader);
-            }
-            if (fragment_shader)
-            {
-                checkShaderCompileStatus(fragment_shader);
-                GL(AttachShader, program, fragment_shader);
-            }
+            GL(AttachShader, program, vertex_shader);
+            GL(AttachShader, program, fragment_shader);
 
             GL(LinkProgram, program);
-            checkShaderProgramLinkStatus(program);
-
-            GL(ValidateProgram, program);
-
-            //if (vertex_shader  ) GL(DetachShader, program, vertex_shader  );
-            //if (fragment_shader) GL(DetachShader, program, fragment_shader);
 
             return program;
         }
