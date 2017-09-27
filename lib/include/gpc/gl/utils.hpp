3838eb3c6b01ddada95fe09a6253feaa588c5904
Jean-Pierre Gygax <gygax@practicomp.ch>
2017-09-27 17:37:51 +0200

    Removed dependency on gl.h

--- lib/include/gpc/gl/utils.hpp
+++ lib/include/gpc/gl/utils.hpp
@@ -7,28 +7,28 @@
 #define WINDOWS_LEAN_AND_MEAN
 #include <Windows.h>
 #endif
-#include <gl/GL.h>
+//#include <gl/GL.h>
 
 
 namespace gpc {
 
     namespace gl {
     
-        typedef GLfloat FloatVec2[2];
-        typedef GLfloat FloatVec3[3];
-        typedef GLfloat FloatVec4[4];
+        typedef float FloatVec2[2];
+        typedef float FloatVec3[3];
+        typedef float FloatVec4[4];
 
-        typedef GLint IntVec2[2];
-        typedef GLint IntVec3[3];
-        typedef GLint IntVec4[4];
+        typedef int32_t IntVec2[2];
+        typedef int32_t IntVec3[3];
+        typedef int32_t IntVec4[4];
 
-        typedef GLfloat Matrix4[4][4];
-        typedef GLfloat Matrix3[3][3];
+        typedef float Matrix4[4][4];
+        typedef float Matrix3[3][3];
 
         struct VertexAttribute {
-            GLenum type;
-            GLint  count;
-            GLint index;
+            uint32_t type; // actually GLenum
+            int32_t  count;
+            int32_t index;
             // TODO: "normalized" field ?
         };
 
